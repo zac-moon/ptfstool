7206e75d3418c0a74e8bccdaba05261d764c7a3a
zac-moon <zacemoon@gmail.com>
2024-02-03 23:24:06 +0000

    js script now reads form input in index

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,1 +1,7 @@
 console.log('Site Launched')
+
+function airportSelected() {
+    console.log('Airport Selected')
+    var airport = document.forms["airportSelectForm"]["airportSelect"].value;
+    console.log('Selected :'+airport)
+}
