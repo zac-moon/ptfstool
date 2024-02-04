e3c09b1f190eec6364c1790e8e9fa2b6e0e449c0
zac-moon <zacemoon@gmail.com>
2024-02-04 00:44:38 +0000

    actual redirection fix

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,8 +1,10 @@
 console.log('Site Launched')
 
 function airportSelected() {
+    event.preventDefault()
     console.log('Airport Selected');
     var airport = document.forms["airportSelectForm"]["airportSelect"].value;
     console.log('Selected :'+airport);
-    window.location.href = 'https://zac-moon.github.io/ptfstool/tool.html';
+    window.location.href = 'tool.html';
+    console.log('redirected')
 }
